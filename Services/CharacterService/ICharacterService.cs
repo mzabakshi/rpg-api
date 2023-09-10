c14f687a24951535e6c5beb10fac0d10eb0642ff
Zakiul Bakshi <zakiul@outlook.com>
2023-09-10 00:25:48 -0700

    Adds method comments

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -7,10 +7,37 @@ namespace rpg_api.Services.CharacterService
 {
     public interface ICharacterService
     {
+        /// <summary>
+        /// Get all characters
+        /// </summary>
+        /// <returns></returns>
         Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters(int userId);
+        
+        /// <summary>
+        /// Gets a single character by id
+        /// </summary>
+        /// <param name="id">int</param>
+        /// <returns></returns>
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+        
+        /// <summary>
+        /// Add a new character
+        /// </summary>
+        /// <param name="character">AddCharacterDto</param>
+        /// <returns>List of Characters</returns>
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto character);
+
+        /// <summary>
+        /// Updates a character
+        /// </summary>
+        /// <param name="updatedCharacterDto">UpdateCharacterDto</param>
         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto character);
+
+        /// <summary>
+        /// Removes a character
+        /// </summary>
+        /// <param name="id">int</param>
+        /// <returns></returns>
         Task<ServiceResponse<List<GetCharacterDto>>> RemoveCharacter(int id);
     }
 }
